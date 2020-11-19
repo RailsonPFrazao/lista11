bd24065ba1cdf218a491788ddbac4c5382b02cbe
Railson Frazão <railsonpontes4@gmail.com>
2020-11-18 23:05:20 -0300

    Novo

--- questao2.sh
+++ questao2.sh
@@ -2,4 +2,12 @@
 
 read -p "Digite o arquivo:" arq
 
-sed 's_\<\(ht\|f\)tp://[^ ]*_<a href="&">&</a>_' $arq
+cp index2.html index.html
+
+cat index.html | sed -E "s/titulo/$(head -n1 $arq)/g" > tmp
+mv tmp index.html
+
+for line in $(sed '1d' < $arq); do
+	cat index.html | sed "/<\/ul>/i<li>$line</li>" > tmp
+	mv tmp index.html
+done
